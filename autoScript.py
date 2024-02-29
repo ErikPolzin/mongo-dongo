bb553194560b5d01ec2ee3aea3c7140bc61b9975
richietaylor <richard@taylorfamily.co.za>
2024-03-01 01:46:13 +0200

    Added all team quesies -b

--- autoScript.py
+++ autoScript.py
@@ -6,8 +6,29 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['healthcare_db'] 
 collection = db['cases']  
 
+# Stuart
+print("Patients sorted by Name:")
+patients_sorted = db.patients.find().sort("Name", 1)
+for patient in patients_sorted:
+    print(patient)
 
-pipeline = [
+# Erik
+print("\nMost common medical condition:")
+most_common_condition = db.cases.aggregate([
+    { "$group": { "_id": "$Medical Condition", "count": { "$sum": 1 } } },
+    { "$sort": {"count": -1} },
+    { "$limit": 1 }
+])
+for condition in most_common_condition:
+    print(condition)
+
+# Ryan
+print("\nDeleting Tiffany Ramirez:")
+result = db.patients.delete_one({"Name": "Tiffany Ramirez"})
+print("Deleted count:", result.deleted_count)
+
+# Richard
+result = [
     {
         "$project": {
             'Medical Condition': 1,
@@ -30,8 +51,11 @@ pipeline = [
     }
 ]
 
-results = collection.aggregate(pipeline)
+results = collection.aggregate(result)
 
 # Printing the results
 for document in results:
     print(document)
+
+#@TODO Bianca
+    
